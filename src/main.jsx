7e82fbf451459acf4f2e2dfa9882804481863eab
Nlucky320 <luckyes144@gmail.com>
2024-03-19 23:18:49 +0600

    nested router

--- src/main.jsx
+++ src/main.jsx
@@ -7,26 +7,26 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './components/Home/Home.jsx';
-import Header from './components/Header/Header.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   },
-  {
-    path:'/header',
-    element: <Header></Header>
-  },
-  {
-    path: '/about',
-    element: <div>Hello from about page</div>
-
-  },
-  {
-    path: '/contact',
-    element: <div>call me right now</div>
-  }
+ 
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
