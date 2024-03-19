956bd22ad1347300b4e4987006a80b303c0deccd
Nlucky320 <luckyes144@gmail.com>
2024-03-19 23:35:52 +0600

    nested routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav>
-            <h1>Navbar</h1> 
-
+      
+<span>My website</span>
             <Link to='/'>Home</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact Us</Link>
+            <Link to="/users">Users</Link>
            
        
         </nav>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 
 const Home = () => {
     return (
         <div>
-            <h1> This is home</h1>
             <Header></Header>
             <Outlet></Outlet>
+        <Footer></Footer>
         </div>
     );
 };
